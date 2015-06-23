2e7742b3b89edf4241539e7ce9f3444468ba2c24
Gregory Barry <gregory.barry@fyber.com>
2015-06-23 15:39:50 +0200

    [CHANGES] Nuke input checks

--- nuke.sh
+++ nuke.sh
@@ -12,11 +12,24 @@ command -v hdfs >/dev/null 2>&1 || {
 	exit 1
 }
 
+command -v kafka-topics >/dev/null 2>&1 || {
+    echo "The kafka-topics command must be defined. Aborting."
+    exit 1
+}
+
 function print_usage() {
-    echo "Usage: `basename $0` <camus_destination_dir> [-d database] [-r repository_uri]"
+    echo "Usage: `basename $0` <camus_destination_dir> [-t topic] [-d database] [-r repository_uri]"
     echo ""
     echo "camus_destination_dir"
     echo "      HDFS path where Camus stores its destination directory."
+    echo ""
+    echo "mode"
+    echo "      Mode selection :"
+    echo "          force, delete data in HDFS."
+    echo "          persist, keep data in HDFS."
+    echo ""
+    echo "-t,--topic <topic>"
+    echo "      name of topic to nuke, default is none"
     echo ""
     echo "-h,--help"
     echo "      prints this message"
@@ -27,13 +40,15 @@ function print_usage() {
     exit 1
 }
 
-function check_sucess() {
+function check_success() {
     local status=$?
     if [[ $status -ne 0 ]]; then
-        echo "HDFS ERROR : Cannot delete data for $TOPIC in HDFS, see $LOG_DIR for more infos."i
+        echo "HDFS ERROR : Cannot delete data for $TOPIC, see $LOG_DIR for more infos." >> $NUKE_STDERR
+        exit 1
+    else
+        return 0
     fi
 }
-
 
 # Process the arguments
 
@@ -45,7 +60,10 @@ if [[ -z "$CAMUS_DESTINATION_DIR" ]]; then
     exit 1
 fi
 
-if [[ -z "$MODE" ]]; then
+MODE=$2
+
+if [[ -z "$MODE" ]]; then #|| "$MODE" != "force" || "$MODE" != "persist" ]]; then
+    echo "MOSSSSSSSSSS"
     print_usage
     exit 1
 fi
@@ -57,11 +75,15 @@ while [[ $# -gt 0 ]]; do
     shift
     current_arg="$1"
     case "$opt" in
+    "-t"|"--topic")
+        TOPIC=$current_arg
+        shift
+        ;;
     "-d"|"--database")
         DATABASE=$current_arg
         shift
         ;;
-   "-h"|"--help")
+    "-h"|"--help")
         print_usage
         exit 0
         ;;
@@ -72,12 +94,46 @@ while [[ $# -gt 0 ]]; do
         ;;
     esac
 done
+
 if [[ -z "$DATABASE" ]]; then
     DATABASE="default"
 fi
-HIVE="hive --database $DATABASE -S"
 
+# Configuration variables
+HIVE="hive --database $DATABASE -S"     # Hive bin
+KAFKA="kafka-topics"                    # Kafka-topic bin
+ZOOKEEPER="xray01.dc1.fyber.com:2181"   # ZK Url
+RETENTION=1000                          # Retention in Ms
 
-sudo -u hdfs hdfs dfs -rm -r -f $CAMUS_DESTINATION_DIR/$TOPIC 
+# Dir and log files
+LOG_DIR="/var/log/nuke"
+NUKE_STDOUT="$LOG_DIR/nuke.log"
+NUKE_STDERR="$LOG_DIR/nuke-stderr.log"
 
+#Pre-requisite
 
+mkdir -p $LOG_DIR
+
+# Here we start the real s**t
+
+${HIVE} -e "DROP table ${TOPIC}" 2>> $NUKE_STDERR
+
+if check_success; then
+    echo `date +%x\ +%X\ ` "HIVE table deleted for topic : ${TOPIC}" >> $NUKE_STDOUT
+
+    ${KAFKA} --alter --zookeeper $ZOOKEEPER --topic $TOPIC --config retention.ms=$RETENTION 2>> $NUKE_STDERR
+    if check_success; then
+        echo `date +%x\ +%X\ ` "Kafka retention changed to : $RETENTION ms for topic : ${TOPIC}" >> $NUKE_STDOUT
+    fi
+fi
+
+if [[ "$MODE" = "force" ]]; then
+    sudo -u hdfs hdfs dfs -rm -r -f $CAMUS_DESTINATION_DIR/$TOPIC 2>> $NUKE_STDERR
+    if check_success; then
+        echo `date +%x\ +%X\ ` "HDFS files deleted for topic : ${TOPIC}" >> $NUKE_STDOUT
+    else
+        echo `date +%x\ +%X\ ` "Some errors occured while deleting data from HDFS for topic : ${TOPIC}"
+    fi
+fi
+
+echo `date +%x\ +%X\ ` "Finished processing topic ${TOPIC} " >> $NUKE_STDOUT
